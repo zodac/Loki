080b582dc5027efc31f65bea214ced1243b0bd1f
= <=>
2014-03-25 12:50:46 +0000

    Updated persistence to add 4 users by default (overwrites entire table) Added files to add test folders to repo.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
--- You can use this file to load seed data into the database using SQL statements
--- insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+INSERT INTO User VALUES('user', 'pass', 'System Administrator');
+INSERT INTO User VALUES('nme', 'pass', 'Network Management Engineer');
+INSERT INTO User VALUES('se', 'pass', 'Support Engineer');
+INSERT INTO User VALUES('csr', 'pass', 'Customer Service Rep');
