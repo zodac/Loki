ebce6a4de3bbfabd5de608d6e63e8f2b42d82521
zodac <arouge110@msn.com>
2014-03-25 12:41:16 +0000

    Updated to work on Linux.

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -3,13 +3,11 @@ package entities;
 import java.io.Serializable;
 import javax.persistence.*;
 
-
 /**
  * The persistent class for the user database table.
  * 
  */
 @Entity
-@Table(name="user")
 @NamedQuery(name="User.findAll", query="SELECT u FROM User u")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
