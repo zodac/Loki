5cd9d2e82c31187730424afb2ae97eee1f54856a
timcog <timcog@gmail.com>
2014-04-05 23:22:23 +0100

    Adding a couple of tests

--- src/test/java/entityTests/CountAndDurationTest.java
+++ src/test/java/entityTests/CountAndDurationTest.java
@@ -27,5 +27,7 @@ public class CountAndDurationTest {
 		cdIMSI.setCount(COUNT_DURATION_IMSI_TEST_COUNT);
 		cdIMSI.setNumofFailures(COUNT_DURATION_IMSI_TEST_NUM_FAILURE);
 		assertEquals(COUNT_DURATION_IMSI_TEST_IMSI, cdIMSI.getIMSI());
+		assertEquals(Long.valueOf(COUNT_DURATION_IMSI_TEST_COUNT), Long.valueOf(cdIMSI.getCount()));
+		assertEquals(Long.valueOf(COUNT_DURATION_IMSI_TEST_NUM_FAILURE),Long.valueOf(cdIMSI.getNumofFailures()));
 	}
 }
